9394e3d67940dc6ca7efc5245d4fd9e0752cbe8a
renanwurster <totozerameister@gmail.com>
2025-04-15 15:24:11 +0200

    add LocalAuthView and HomeView

---
+++ LocalBiometricAuth/Views/HomeView.swift
@@ -0,0 +1,38 @@
+//
+//  HomeView.swift
+//  LocalBiometricAuth
+//
+//  Created by Renan Wurster on 15.04.25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject private var localAuthViewModel: LocalAuthViewModel
+    
+    var body: some View {
+        VStack{
+            Text("Welcome to the Home View!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+                .padding(.bottom, 30)
+            
+            Button("Logout"){
+                localAuthViewModel.isUnlocked = false
+            }
+            .font(.title2)
+            .fontWeight(.semibold)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.red)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+        .environmentObject(LocalAuthViewModel())
+}
---
+++ LocalBiometricAuth/Views/LocalAuthView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  LocalBiometricAuth
+//
+//  Created by Renan Wurster on 15.04.25.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+
+struct LocalAuthView: View {
+    @State private var isAuthorize = false
+    @EnvironmentObject private var localAuthViewModel : LocalAuthViewModel
+
+    
+    var body: some View {
+        if localAuthViewModel.isUnlocked {
+            HomeView()
+        } else {
+            VStack{
+                Text("Please authenticate to continue.")
+                    .font(.headline)
+                    .padding()
+                
+                if let error = localAuthViewModel.errorMessage {
+                    Text(error)
+                        .foregroundColor(.red)
+                        .padding()
+                    
+                    Button("Authenticate") {
+                        Task {
+                            await localAuthViewModel.authenticate()
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding()
+                }
+            }
+            .task {
+                await localAuthViewModel.authenticate()
+            }
+        }
+        
+    }
+}
+
+
+#Preview {
+    LocalAuthView()
+        .environmentObject(LocalAuthViewModel())
+}
