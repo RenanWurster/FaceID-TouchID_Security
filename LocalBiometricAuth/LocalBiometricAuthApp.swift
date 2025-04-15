d7b6b2d55b86d07d44a402c66f58e9f90f9219ec
renanwurster <totozerameister@gmail.com>
2025-04-15 15:07:53 +0200

    Initial Commit

---
+++ LocalBiometricAuth/LocalBiometricAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalBiometricAuthApp.swift
+//  LocalBiometricAuth
+//
+//  Created by Renan Wurster on 15.04.25.
+//
+
+import SwiftUI
+
+@main
+struct LocalBiometricAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
